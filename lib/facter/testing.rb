823ca57d3e5518455a06756a6ec0e6ca7ee3ff88
kumar <kumar@puppet.com>
2023-08-07 13:43:57 +0800

    added new facts with confine test

--- lib/facter/testing.rb
+++ lib/facter/testing.rb
@@ -63,3 +63,17 @@ Facter.add(:az_portfolio) do
     end
   end
 end
+
+Facter.add(:az_port) do
+  confine :cloud_provider do |value|
+    value == 'azure'
+  end
+
+  setcode do
+    if Facter.value(:osfamily) == 'windows'
+      'windows'
+    elsif Facter.value(:osfamily) == 'RedHat'
+      'redhat'
+    end
+  end
+end
