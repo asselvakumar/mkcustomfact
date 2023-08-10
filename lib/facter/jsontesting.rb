3f681f24d572ca8ecd9e40549a7d2a0d878bde00
kumar <kumar@puppet.com>
2023-08-10 22:04:29 +0800

    added new facts with confine test

--- lib/facter/jsontesting.rb
+++ lib/facter/jsontesting.rb
@@ -5,10 +5,10 @@ $portfolio_value = ''
 
 # Convert JSON-like string to a Puppet data structure
 $tagsList = Facter.value('az_metadata.compute.tagsList')
-$tags = JSON.parse($tagsList.gsub('=>', ':'))
+#$tags = JSON.parse($tagsList.gsub('=>', ':'))
 
 # Loop through the tags and find the "portfolio" value
-$tags.each |$tag| {
+$tagsList.each |$tag| {
   if $tag['name'] == 'portfolio' 
     $portfolio_value = $tag['value']
     break
