baed39e10b6fd44a83847ed549fe89aa8de132f9
kumar <kumar@puppet.com>
2023-08-10 14:49:33 +0800

    added new facts with confine test

--- lib/facter/testing.rb
+++ lib/facter/testing.rb
@@ -62,22 +62,23 @@ Facter.add(:az_portfolio) do
   end
 end
 
-Facter.add(:find_portfolio) do
-  setcode do
-    portfolio_value = ''
-      # Convert JSON-like string to a Puppet data structure
-      tagsList = Facter.value('az_metadata.compute.tagsList')
-      tags = JSON.parse(tagsList.gsub('=>', ':'))
+#Facter.add(:find_portfolio) do
+#  setcode do
+#    portfolio_value = ''
+#      # Convert JSON-like string to a Puppet data structure
+#      tagsList = Facter.value('az_metadata.compute.tagsList')
+#      tags = JSON.parse(tagsList.gsub('=>', ':'))
+#
+#      # Loop through the tags and find the "portfolio" value
+#      tags.each do |tag| 
+#        if tag['name'] == 'portfolio' 
+#          portfolio_value = tag['value']
+#          break
+#        end
+#      end
+#      
+#      portfolio_value
+#  end
+#end
 
-      # Loop through the tags and find the "portfolio" value
-      tags.each do |tag| 
-        if tag['name'] == 'portfolio' 
-          portfolio_value = tag['value']
-          break
-        end
-      end
-      
-      portfolio_value
-  end
-end
 
