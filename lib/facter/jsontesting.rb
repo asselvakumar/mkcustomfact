9978c6d70c48fa5f79003853ad4398fae6a29b02
kumar <kumar@puppet.com>
2023-08-11 11:16:52 +0800

    added new facts with confine test

--- lib/facter/jsontesting.rb
+++ lib/facter/jsontesting.rb
@@ -3,21 +3,8 @@ require 'json'
 Facter.add(:portfolio_value) do
     setcode do
       tagsList_json = Facter.value('az_metadata.compute.tagsList')
-  
-      if tagsList_json
-        tagsList = JSON.parse(tagsList_json)
-        portfolio_value = ''
-  
-        tagsList.each do |tag|
-          if tag['name'] == 'portfolio'
-            portfolio_value = tag['value']
-            break
-          end
+      tagsList_json.each do |$key, $value|
+            notice($value)
         end
-  
-        portfolio_value
-      else
-        nil
-      end
     end
-  end
+end
