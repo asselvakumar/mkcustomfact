1ce49eafb3e3c97a6ca3ad158de789fdcb881e05
kumar <kumar@puppet.com>
2023-09-11 09:53:26 +0800

    added new ruby task

---
+++ tasks/facttest.rb
@@ -0,0 +1,13 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+# Puppet Task Name: facttest
+#
+# This is where you put the ruby code for your task.
+#
+# You can write Puppet tasks in any language you want and it's easy to
+# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
+# https://puppet.com/docs/bolt/0.x/writing_tasks.html
+#
+
+kernel = system('facter -p kernel')
+puts "My kernel is #{kernel}"
