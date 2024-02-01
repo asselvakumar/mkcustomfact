f4ad3cb6a9349a8b4eb8da9399c13417b11f40d2
kumar <kumar@puppet.com>
2024-02-01 17:11:35 +0800

    added new class

---
+++ manifests/internet_explorer.pp
@@ -0,0 +1,19 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include mkcustomfact::internet_explorer
+class mkcustomfact::internet_explorer (
+  Boolean $esc_enabled_admin = false,
+  Boolean $esc_enabled_user = false,
+){
+  dsc_ieenhancedsecurityconfiguration { 'esc_enabled_admin' :
+    dsc_enabled => $esc_enabled_admin,
+    dsc_role    => 'Administrators',
+  }
+  dsc_ieenhancedsecurityconfiguration { 'esc_enabled_user' :
+    dsc_enabled => $esc_enabled_user,
+    dsc_role    => 'Users',
+  }
+}
