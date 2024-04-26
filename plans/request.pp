1f0f444a12dd54b515bf350cd5503877949ac599
kumar <kumar@puppet.com>
2024-04-26 16:14:32 +0800

    updated the code to comment out the notify resource

--- plans/request.pp
+++ plans/request.pp
@@ -2,6 +2,8 @@ plan mkcustomfact::request (
   TargetSpec $targets
 ) {
   $api_key = lookup('mkcustomfact::api_key')
+  notify {"Running with api_key ${api_key} ID defined":}
+
   #$result  = run_task('mkcustomfact', $targets)
   $result  = run_task('mkcustomfact', $targets, 'api_key' => $api_key)
   return $result
